e1e92208d48bf3af535e6c31e490dddffff86f32
AngelValay <1730322@upv.edu.mx>
2018-06-20 18:07:05 -0500

    Actualizacion de la conexion

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php 
 	$contrasena = "alu1234";
-	$user = "alu";
-	$nombre_bd = "reg";
+	$user = "u332418796_alu";
+	$nombre_bd = "u332418796_reg";
 	try {
 		$bd = new PDO('mysql:host=mysql.hostinger.mx;dbname='.$nombre_bd,$user,$contrasena);
 	} catch (Exception $e) {
